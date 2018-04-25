f51403d442ed967520fadc90aebca056ba01b69f
carboni-rob <carboni@robertocarboni.it>
2018-04-25 17:45:40 +0100

    First commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/searchBar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyDjIq8qmATSqfbg_N2KnS6k5ELZ8K4w_6Q';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
